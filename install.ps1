0fa0caade94f6af1ac9f56840a128b58d592172c
sbrandsen <45394133+sbrandsen@users.noreply.github.com>
2023-01-17 23:38:51 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -12,41 +12,41 @@ $totalSteps = 5
 $currentStep = 1
 
 # Download the zip file
-Write-Host -Activity "Downloading zip file" -Status "Step $currentStep of $totalSteps" -PercentComplete (($currentStep / $totalSteps) * 100)
+Write-Progress -Activity "Downloading zip file" -Status "Step $currentStep of $totalSteps" -PercentComplete (($currentStep / $totalSteps) * 100)
 Invoke-WebRequest -Uri "https://github.com/sbrandsen/LightApprenticeServer/archive/refs/heads/main.zip" -OutFile "$env:USERPROFILE\Downloads\zipfile.zip"
 
 # Update the current step variable
 $currentStep++
 
 # Unzip the file
-Write-Host -Activity "Unzipping file" -Status "Step $currentStep of $totalSteps" -PercentComplete (($currentStep / $totalSteps) * 100)
+Write-Progress -Activity "Unzipping file" -Status "Step $currentStep of $totalSteps" -PercentComplete (($currentStep / $totalSteps) * 100)
 Expand-Archive -Force -Path "$env:USERPROFILE\Downloads\zipfile.zip" -DestinationPath "$env:USERPROFILE\Downloads\"
 
 # Update the current step variable
 $currentStep++
 
 # Change directory to the unzipped folder
-Write-Host -Activity "Changing directory" -Status "Step $currentStep of $totalSteps" -PercentComplete (($currentStep / $totalSteps) * 100)
+Write-Progress -Activity "Changing directory" -Status "Step $currentStep of $totalSteps" -PercentComplete (($currentStep / $totalSteps) * 100)
 cd "$env:USERPROFILE\Downloads\LightApprenticeServer-main"
 
 # Update the current step variable
 $currentStep++
 
 # Copy the "Apprenticeserver" folder to programdata
-Write-Host -Activity "Copying files" -Status "Step $currentStep of $totalSteps" -PercentComplete (($currentStep / $totalSteps) * 100)
+Write-Progress -Activity "Copying files" -Status "Step $currentStep of $totalSteps" -PercentComplete (($currentStep / $totalSteps) * 100)
 Copy-Item -Force -Recurse -Path "Apprenticeserver" -Destination "C:\ProgramData"
 
 # Update the current step variable
 $currentStep++
 
 # Remove the unzipped folder
-Write-Host -Activity "Removing files" -Status "Step $currentStep of $totalSteps" -PercentComplete (($currentStep / $totalSteps) * 100)
+Write-Progress -Activity "Removing files" -Status "Step $currentStep of $totalSteps" -PercentComplete (($currentStep / $totalSteps) * 100)
 cd ..
 Remove-Item -Path "$env:USERPROFILE\Downloads\LightApprenticeServer-main" -Recurse -Force
 Remove-Item -Path "$env:USERPROFILE\Downloads\zipfile.zip" -Recurse -Force
 
 # Display a message when the script is done
-Write-Host -Activity "Completed" -Status "All steps have been completed" -Completed
+Write-Progress -Activity "Completed" -Status "All steps have been completed" -Completed
 Write-Host "All steps have been completed"
 
 Write-Host ""
@@ -57,3 +57,5 @@ if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Classes\CLSID\{C343ED84-A129-11d3-B79
 New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\CLSID\{C343ED84-A129-11d3-B799-0060B0F159EF}' -Name '(default)' -Value 'Inventor: Apprentice Server Component' -PropertyType String -Force -ea SilentlyContinue;
 New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\CLSID\{C343ED84-A129-11d3-B799-0060B0F159EF}\InprocServer32' -Name '(default)' -Value 'C:\ProgramData\ApprenticeServer\RxApprenticeServer.dll' -PropertyType String -Force -ea SilentlyContinue;
 New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Classes\CLSID\{C343ED84-A129-11d3-B799-0060B0F159EF}\InprocServer32' -Name 'ThreadingModel' -Value 'Apartment' -PropertyType String -Force -ea SilentlyContinue;
+
+Read-Host -Prompt "Apprentice Server is installed" 
